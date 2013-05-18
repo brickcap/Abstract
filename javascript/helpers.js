3285e8d0aa1f7edf9d81401f1bec29a77c37c0e7
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-05-18 18:28:56 +0530

    Refactored the script files into two.

---
+++ javascript/helpers.js
@@ -0,0 +1,107 @@
+function loadSavedDrafts(){
+        return Object.keys(localStorage);
+    }
+    
+    function sortedArray (data){
+        
+        return data.sort(function(a,b){
+        a = new Date(a.trueDate);
+        b = new Date(b.trueDate);
+        return a<b?-1:a>b?1:0;
+        }).reverse();
+        
+    }
+    
+    function buildData(keys){
+        
+        var data = [];
+        for(var i =0;i< keys.length; i++){
+            var parsed = JSON.parse(localStorage[keys[i]]);
+            var draft = {};
+            var count = parsed.wordCount;
+            draft["date"] = new Date(parsed.time).toDateString();
+            draft["count"] = count;
+            draft["title"] = keys[i];
+            draft["plural"] = count > 1 ; 
+            draft["trueDate"] = parsed.time;
+            data.push(draft);
+        }
+        
+        return sortedArray(data);
+        
+}
+
+function renderSavedDrafts(){
+        
+        
+        var data = {};
+        data["keys"] = buildData(loadSavedDrafts());
+        var template = "{{#keys}}<li data-title='{{title}}' class='draftListItems'><p>{{title}} <span class='muted draftList'> ({{date}})</span></p> <p class='muted draftList '> {{count}} word{{#plural}}s{{/plural}}</p>  </li><hr/>{{/keys}}";
+        var html = Mustache.render(template,data);
+        $("#draftList").html(html);
+    }
+    
+    function prepareInitialWorkSpace(){
+    
+    var editArea = $("#editArea");
+    editArea.autosize();
+    $("#title").focus();
+    
+    return editArea;
+        
+    }
+    function hideThis(elements){
+        
+        for(var i = 0; i<elements.length; i++){
+            
+        $(elements[i]).hide();
+    }
+}
+    
+    function showThis(elements){
+        
+        for(var i = 0; i<elements.length; i++){
+            
+        $(elements[i]).show();
+    }
+}
+    
+    function getMarkdownText(){
+        
+        return $("#editArea").val();
+    }
+    
+    function getWordCount(text){
+        
+     return text.split(/\s+\b/).length;
+    }
+    
+    function setHtmlinPreviewPane(markdownText){
+        $("#wordCount").text('words: '+getWordCount(markdownText));
+        $("#previewPane").html(markdown.toHTML(markdownText));
+    }
+    
+    function setRawHtml(){
+        var pane = $("#previewPane");
+        pane.text(pane.html());
+    }
+    
+    function setPlain(){
+        
+        var pane = $("#previewPane");
+        pane.html(pane.text());
+    }
+    function saveCurrentDraft(){
+        
+        var key = $("#title").text();
+        var draft = {};
+        draft["time"] = new Date();
+        draft["text"] = getMarkdownText();
+        draft["wordCount"] = getWordCountFromLabel($("#wordCount").text());
+        localStorage.setItem(key, JSON.stringify(draft));
+    }
+
+function getWordCountFromLabel(text){
+        
+        return text.match(/\d+/)[0];
+    }
