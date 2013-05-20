1f55949c6c4830a13f4f7b5cfdd1a04fc84463c7
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-05-20 21:29:53 +0530

    View models moved to index file as well

--- javascript/index.js
+++ javascript/index.js
@@ -1,4 +1,37 @@
     $(function () {
+        
+        
+    var draft = function(parsed,title){
+    
+    var self = this;
+    var wordCount = parsed.wordCount;
+    self.date = new Date(parsed.time).toDateString();
+    self.count = wordCount;
+    self.title = title;
+    self.plural = wordCount > 1;
+    self.trueDate = new Date(parsed.time);
+    
+};
+
+
+
+var viewModel = function(drafts){
+    
+     var self = this;
+     
+     self.drafts = ko.observableArray(drafts);
+    
+    self.deleteDraft = function(draft,event){
+        event.stopPropagation();
+        removeDraft(draft.title);
+       self.drafts.remove(draft);        
+    }
+ };
+
+
+
+
+
 
 
 
