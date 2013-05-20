bc2105d2c75d7f4c6070f79a5d75ddcbe34c2800
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-05-20 21:20:42 +0530

    Everything moved back to one file
    Should have used require.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,128 @@
     $(function(){
     
+       
+    
+    function prepareInitialWorkSpace(){
+    
+    var editArea = $("#editArea");
+    editArea.autosize();
+    $("#title").focus();    
+    return editArea;
         
+    }
+
+    function hideThis(elements){
+        
+        for(var i = 0; i<elements.length; i++){
+            
+        $(elements[i]).hide();
+    }
+}
+    
+    function showThis(elements){
+        
+        for(var i = 0; i<elements.length; i++){
+            
+        $(elements[i]).show();
+}
+    }
+    
+    function getMarkdownText(){
+        
+        return $("#editArea").val();
+    }
+    
+    function getWordCount(text){
+        
+     return text.split(/\s+\b/).length;
+    }
+    
+    function setHtmlinPreviewPane(markdownText){
+        $("#wordCount").text('words: '+getWordCount(markdownText));
+        $("#previewPane").html(markdown.toHTML(markdownText));
+    }
+    
+    function setRawHtml(){
+        var pane = $("#previewPane");
+        pane.text(pane.html());
+    }
+    
+    function setPlain(){
+        
+        var pane = $("#previewPane");
+        pane.html(pane.text());
+    }
+
+        
+    function getWordCountFromLabel(text){
+        
+        return text.match(/\d+/)[0];
+    }
+
+
+function validateInputOnFousOut(){
+    
+  var isTitleEmpty = $("#title").text().trim()=== '';
+  var isDraftEmpty = $("#editArea").val() === '';
+  var hasTitileAndDraft = !isTitleEmpty && !isDraftEmpty;
+  return hasTitileAndDraft;    
+           
+}
+        
+        var initializeDrafts = new viewModel();
+ko.applyBindings(initializeDrafts);
+
+function loadSavedDrafts(){
+        return Object.keys(localStorage);
+    }
+    
+    function sortedArray (data){
+        
+        return data.sort(function(a,b){
+        a = new Date(a.trueDate);
+        b = new Date(b.trueDate);
+        return a<b?-1:a>b?1:0;
+        }).reverse();
+        
+    }
+    
+    function buildData(keys){
+        
+        var data = [];
+        for(var i =0;i< keys.length; i++){
+            var parsed = JSON.parse(localStorage[keys[i]]);
+            var initializeDraft = new draft(parsed, keys[i]);
+            data.push(initializeDraft);
+        }
+        
+        return sortedArray(data);
+        
+    }
+    
+    function renderSavedDrafts(){        
+        var array = buildData(loadSavedDrafts());
+        initializeDrafts.drafts(array);
+    }
+
+function saveCurrentDraft(){
+        
+        var key = $("#title").text();
+        var draft = {};
+        draft["time"] = new Date();
+        draft["text"] = getMarkdownText();
+        draft["wordCount"] = getWordCountFromLabel($("#wordCount").text());
+        localStorage.setItem(key, JSON.stringify(draft));
+    }
+
+function getDraftFromKey(key){
+    
+    return localStorage.getItem(key);
+}
+
+function removeDraft(key){
+    
+    localStorage.removeItem(key);
+}
         
     var editArea = prepareInitialWorkSpace();
     
