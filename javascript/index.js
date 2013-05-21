dc2df19bade8f620ec9a235af163973c9522d502
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-05-21 13:48:54 +0530

    beautified javascript

--- javascript/index.js
+++ javascript/index.js
@@ -1,232 +1,233 @@
 $(function () {
 
 
-var draft = function(parsed,title){
+    var draft = function (parsed, title) {
 
-var self = this;
-var wordCount = parsed.wordCount;
-self.date = new Date(parsed.time).toDateString();
-self.count = wordCount;
-self.title = title;
-self.plural = wordCount > 1;
-self.trueDate = new Date(parsed.time);
+        var self = this;
+        var wordCount = parsed.wordCount;
+        self.date = new Date(parsed.time).toDateString();
+        self.count = wordCount;
+        self.title = title;
+        self.plural = wordCount > 1;
+        self.trueDate = new Date(parsed.time);
 
-};
-    
-var viewModel = function(drafts){
+    };
 
-var self = this;
-self.drafts = ko.observableArray(drafts);
-self.showEditor = ko.observable(true);
-self.showTitle = ko.observable(true);
-self.raw = ko.observable(true);    
+    var viewModel = function (drafts) {
 
-self.deleteDraft = function(draft,event){
-    
-event.stopPropagation();
-removeDraft(draft.title);
-self.drafts.remove(draft);
-    
-};
+        var self = this;
+        self.drafts = ko.observableArray(drafts);
+        self.showEditor = ko.observable(true);
+        self.showTitle = ko.observable(true);
+        self.raw = ko.observable(true);
 
-self.showDrafts = function(){
-    
-self.showEditor(false);
-self.showTitle(false);
-renderSavedDrafts();
-hideThis(["#previewContainer"]);
-showThis(["#drafts"]);
-    
-};
+        self.deleteDraft = function (draft, event) {
 
-self.newDraft = function(){
-    
-hideThis(["#previewContainer", "#drafts"]);
-self.showTitle(true);
-self.showEditor(true);
-editArea.focus();
-editArea.val('');
-$("#title").text('');
-    
-};
+            event.stopPropagation();
+            removeDraft(draft.title);
+            self.drafts.remove(draft);
 
-self.showPreview = function(){
+        };
 
-if (validateInputOnFousOut()) {
+        self.showDrafts = function () {
 
-    setHtmlinPreviewPane(getMarkdownText());
-    hideThis(['#plain']);
-    self.showEditor(false);
-    self.showTitle(true);
-    showThis(["#rawHtml", '#previewContainer']);
-    saveCurrentDraft();
-    $("#saveStatus").fadeIn().show().delay(1000).fadeOut();
-}
+            self.showEditor(false);
+            self.showTitle(false);
+            renderSavedDrafts();
+            hideThis(["#previewContainer"]);
+            showThis(["#drafts"]);
 
-};
+        };
 
-self.hidePreview = function(){
-    
-hideThis(["#previewContainer"]);
-self.showEditor(true);
-editArea.trigger('autosize');
-editArea.focus();
+        self.newDraft = function () {
 
-};
+            hideThis(["#previewContainer", "#drafts"]);
+            self.showTitle(true);
+            self.showEditor(true);
+            editArea.focus();
+            editArea.val('');
+            $("#title").text('');
 
-self.editDraft = function(draft){
+        };
 
-var title = draft.title;
-var item = getDraftFromKey(title);
-var parsed = JSON.parse(item);
-hideThis(["#drafts"]);
-editArea.val(parsed.text).trigger('autosize');
-$("#title").text(title);
-$("#wordCount").text(parsed.wordCount);
-self.showEditor(true);
-self.showTitle(true);
+        self.showPreview = function () {
 
-};
-};
-function prepareInitialWorkSpace() {
+            if (validateInputOnFousOut()) {
 
-var editArea = $("#editArea");
-editArea.autosize();
-$("#title").focus();
-return editArea;
+                setHtmlinPreviewPane(getMarkdownText());
+                hideThis(['#plain']);
+                self.showEditor(false);
+                self.showTitle(true);
+                showThis(["#rawHtml", '#previewContainer']);
+                saveCurrentDraft();
+                $("#saveStatus").fadeIn().show().delay(1000).fadeOut();
+            }
 
-}
+        };
 
-function hideThis(elements) {
+        self.hidePreview = function () {
 
-for (var i = 0; i < elements.length; i++) {
+            hideThis(["#previewContainer"]);
+            self.showEditor(true);
+            editArea.trigger('autosize');
+            editArea.focus();
 
-    $(elements[i]).hide();
-}
-}
+        };
 
-function showThis(elements) {
+        self.editDraft = function (draft) {
 
-for (var i = 0; i < elements.length; i++) {
+            var title = draft.title;
+            var item = getDraftFromKey(title);
+            var parsed = JSON.parse(item);
+            hideThis(["#drafts"]);
+            editArea.val(parsed.text).trigger('autosize');
+            $("#title").text(title);
+            $("#wordCount").text(parsed.wordCount);
+            self.showEditor(true);
+            self.showTitle(true);
 
-    $(elements[i]).show();
-}
-}
+        };
+    };
 
-function getMarkdownText() {
+    function prepareInitialWorkSpace() {
 
-return $("#editArea").val();
-}
+        var editArea = $("#editArea");
+        editArea.autosize();
+        $("#title").focus();
+        return editArea;
 
-function getWordCount(text) {
+    }
 
-return text.split(/\s+\b/).length;
-}
+    function hideThis(elements) {
 
-function setHtmlinPreviewPane(markdownText) {
-$("#wordCount").text('words: ' + getWordCount(markdownText));
-$("#previewPane").html(markdown.toHTML(markdownText));
-}
+        for (var i = 0; i < elements.length; i++) {
 
-function setRawHtml() {
-var pane = $("#previewPane");
-pane.text(pane.html());
-}
+            $(elements[i]).hide();
+        }
+    }
 
-function setPlain() {
+    function showThis(elements) {
 
-var pane = $("#previewPane");
-pane.html(pane.text());
-}
+        for (var i = 0; i < elements.length; i++) {
 
+            $(elements[i]).show();
+        }
+    }
 
-function getWordCountFromLabel(text) {
+    function getMarkdownText() {
 
-return text.match(/\d+/)[0];
-}
+        return $("#editArea").val();
+    }
+
+    function getWordCount(text) {
+
+        return text.split(/\s+\b/).length;
+    }
+
+    function setHtmlinPreviewPane(markdownText) {
+        $("#wordCount").text('words: ' + getWordCount(markdownText));
+        $("#previewPane").html(markdown.toHTML(markdownText));
+    }
+
+    function setRawHtml() {
+        var pane = $("#previewPane");
+        pane.text(pane.html());
+    }
+
+    function setPlain() {
+
+        var pane = $("#previewPane");
+        pane.html(pane.text());
+    }
 
 
-function validateInputOnFousOut() {
+    function getWordCountFromLabel(text) {
 
-var isTitleEmpty = $("#title").text().trim() === '';
-var isDraftEmpty = $("#editArea").val() === '';
-var hasTitileAndDraft = !isTitleEmpty && !isDraftEmpty;
-return hasTitileAndDraft;
+        return text.match(/\d+/)[0];
+    }
 
-}
 
-var initializeDrafts = new viewModel();
-ko.applyBindings(initializeDrafts);
+    function validateInputOnFousOut() {
 
-function loadSavedDrafts() {
-return Object.keys(localStorage);
-}
+        var isTitleEmpty = $("#title").text().trim() === '';
+        var isDraftEmpty = $("#editArea").val() === '';
+        var hasTitileAndDraft = !isTitleEmpty && !isDraftEmpty;
+        return hasTitileAndDraft;
 
-function sortedArray(data) {
+    }
 
-return data.sort(function (a, b) {
-    a = new Date(a.trueDate);
-    b = new Date(b.trueDate);
-    return a < b ? -1 : a > b ? 1 : 0;
-}).reverse();
+    var initializeDrafts = new viewModel();
+    ko.applyBindings(initializeDrafts);
 
-}
+    function loadSavedDrafts() {
+        return Object.keys(localStorage);
+    }
 
-function buildData(keys) {
+    function sortedArray(data) {
 
-var data = [];
-for (var i = 0; i < keys.length; i++) {
-    var parsed = JSON.parse(localStorage[keys[i]]);
-    var initializeDraft = new draft(parsed, keys[i]);
-    data.push(initializeDraft);
-}
+        return data.sort(function (a, b) {
+            a = new Date(a.trueDate);
+            b = new Date(b.trueDate);
+            return a < b ? -1 : a > b ? 1 : 0;
+        }).reverse();
 
-return sortedArray(data);
+    }
 
-}
+    function buildData(keys) {
 
-function renderSavedDrafts() {
-var array = buildData(loadSavedDrafts());
-initializeDrafts.drafts(array);
-}
+        var data = [];
+        for (var i = 0; i < keys.length; i++) {
+            var parsed = JSON.parse(localStorage[keys[i]]);
+            var initializeDraft = new draft(parsed, keys[i]);
+            data.push(initializeDraft);
+        }
 
-function saveCurrentDraft() {
+        return sortedArray(data);
 
-var key = $("#title").text();
-var draft = {};
-draft["time"] = new Date();
-draft["text"] = getMarkdownText();
-draft["wordCount"] = getWordCountFromLabel($("#wordCount").text());
-localStorage.setItem(key, JSON.stringify(draft));
-}
+    }
 
-function getDraftFromKey(key) {
+    function renderSavedDrafts() {
+        var array = buildData(loadSavedDrafts());
+        initializeDrafts.drafts(array);
+    }
 
-return localStorage.getItem(key);
-}
+    function saveCurrentDraft() {
 
-function removeDraft(key) {
+        var key = $("#title").text();
+        var draft = {};
+        draft["time"] = new Date();
+        draft["text"] = getMarkdownText();
+        draft["wordCount"] = getWordCountFromLabel($("#wordCount").text());
+        localStorage.setItem(key, JSON.stringify(draft));
+    }
 
-localStorage.removeItem(key);
-}
+    function getDraftFromKey(key) {
 
-var editArea = prepareInitialWorkSpace();
+        return localStorage.getItem(key);
+    }
 
-$("#rawHtml").click(function (e) {
+    function removeDraft(key) {
 
-setRawHtml();
-e.stopPropagation();
-hideThis([this]);
-showThis(["#plain"]);
-});
+        localStorage.removeItem(key);
+    }
 
-$("#plain").click(function (e) {
+    var editArea = prepareInitialWorkSpace();
 
-setPlain();
-e.stopPropagation();
-hideThis([this]);
-showThis(["#rawHtml"]);
-});
+    $("#rawHtml").click(function (e) {
+
+        setRawHtml();
+        e.stopPropagation();
+        hideThis([this]);
+        showThis(["#plain"]);
+    });
+
+    $("#plain").click(function (e) {
+
+        setPlain();
+        e.stopPropagation();
+        hideThis([this]);
+        showThis(["#rawHtml"]);
+    });
 
 
 
