4db07ad5146ed3335efece3de9bd1b84b9a8fa7c
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-05-24 20:10:44 +0530

    Uglification process complete

--- javascript/build.js
+++ javascript/build.js
@@ -1,6 +1,7 @@
 var buildify = require('buildify');
 
 buildify()
-  .concat(['StaticVariables.js', 'models.js','helpers.js','drafts.js']).wrap('base.js')
-  .save('./index.js')
+  .concat(['StaticVariables.js', 'models.js','helpers.js','drafts.js']).wrap('base.js').save('./index.js')
+.uglify().save('./index.min.js');
+  
 
